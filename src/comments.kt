54813f1fc577ec6016fc7cff351453eceb5fcee7
Dalia-Ramadan <115746100+Dalia-Ramadan@users.noreply.github.com>
2024-06-29 22:57:40 +0300

    commit

---
+++ src/comments.kt
@@ -0,0 +1,14 @@
+fun main(){
+    //single line comment
+
+    /*
+    multi line comment
+    */
+
+    /*
+    * multi line comment
+    * /*nested comment*/
+    //nested comment
+    */
+    print("hello world")
+}
