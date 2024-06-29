3c79b47c82afaf74d6e7756f33084bb6596701de
Dalia-Ramadan <115746100+Dalia-Ramadan@users.noreply.github.com>
2024-06-29 23:13:43 +0300

    commit0

---
+++ src/Variables.kt
@@ -0,0 +1,20 @@
+fun main(){
+    var Name = "Dalia Ramadan"
+    println(Name)
+    /////////////////////////////////////////
+    var Age = 19
+    println(Age)
+    ////////////////////////////////////////
+    println("My name is $Name , My age = $Age ")
+    ////////////////////////////////////////
+    Name = "Mohamed"
+    Age = 16
+    println("My name is $Name , My age = $Age ")
+    ////////////////////////////////////////
+    Name = ""
+    println(Name.isEmpty())
+    ////////////////////////////////////////
+    Name = "M"
+    println(Name.isEmpty())
+
+}
