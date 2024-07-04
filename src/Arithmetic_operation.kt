1f9c2d165047152d3478f304d1262ecc6ac0ec57
Dalia-Ramadan <115746100+Dalia-Ramadan@users.noreply.github.com>
2024-07-04 06:57:22 +0300

    commit0

--- src/Arithmetic_operation.kt
+++ src/Arithmetic_operation.kt
@@ -2,6 +2,7 @@ fun main(){
     val num1 = 1.5
     val num2 = 2.5
     var res:Double
+    //arithmetic operations
     res = num1 + num2
     println(res)
     //////////////////////////////////////
@@ -16,4 +17,11 @@ fun main(){
     /////////////////////////////////////
     res = num2 % num1
     println(res)
+    /////////////////////////////////////
+    println("/////////////////////////////////////////////////")
+    //concatenation of string
+    val name1:String = "Dalia"
+    val name2:String = "Ramadan"
+    var MyName:String = name1 + " " + name2
+    println(MyName)
 }
