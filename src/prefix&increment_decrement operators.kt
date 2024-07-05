f0fd391c3467b98da331f06486d9af8fdc204a13
Dalia-Ramadan <115746100+Dalia-Ramadan@users.noreply.github.com>
2024-07-05 23:33:20 +0300

    commit0

---
+++ src/prefix&increment_decrement operators.kt
@@ -0,0 +1,19 @@
+package operators
+/*
+unary plus --> (+a)
+unary minus --> (-a)
+not --> (!a)
+increment --> (++a)
+decrement --> (--a)
+ */
+fun main(){
+    val a = 5
+    var res = +a
+    println(res)
+    ////////////////////////////////////
+    res = -a
+    println(res)
+    ///////////////////////////////////
+    res = !a
+    println(res)
+}
