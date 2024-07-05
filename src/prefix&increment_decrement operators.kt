215425d34eb2cee9f0b9af9d355f43cc38e68685
Dalia-Ramadan <115746100+Dalia-Ramadan@users.noreply.github.com>
2024-07-05 23:47:39 +0300

    commit0

--- src/prefix&increment_decrement operators.kt
+++ src/prefix&increment_decrement operators.kt
@@ -8,12 +8,24 @@ decrement --> (--a)
  */
 fun main(){
     val a = 5
+    var b = 4
+    var c = 6
+
     var res = +a
     println(res)
+
     ////////////////////////////////////
+
     res = -a
     println(res)
+
     ///////////////////////////////////
-    res = !a
-    println(res)
+
+    var res1 = ++b
+    println(res1)
+
+    /////////////////////////////////////
+
+    var res2 = --c
+    println(res2)
 }
